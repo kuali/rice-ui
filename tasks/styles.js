75428a8cf604f7ebf6d52df6dd68431089e541ba
Chris Basham <csbasham@iu.edu>
2014-09-15 15:21:03 -0400

    Added Autoprefixer.

--- tasks/styles.js
+++ tasks/styles.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
+var prefix = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var proxy = require('browser-sync-instance')('proxy');
 
 gulp.task('styles', function() {
   return gulp.src('src/**/*.css')
+    .pipe(prefix())
     .pipe(concat('bundle.css'))
     .pipe(gulp.dest('./dist/assets'))
     .pipe(proxy.reload({ stream: true }));
