df459835e3cd5ba8f05e381236afc44c84fe8034
Chris Basham <csbasham@iu.edu>
2014-09-15 17:05:53 -0400

    Added Less preprocessor.

--- tasks/styles.js
+++ tasks/styles.js
@@ -1,10 +1,23 @@
 var gulp = require('gulp');
+var merge = require('merge-stream');
+var less = require('gulp-less');
 var prefix = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var proxy = require('browser-sync-instance')('proxy');
 
 gulp.task('styles', function() {
-  return gulp.src('src/**/*.css')
+
+  var vendor = gulp.src([
+      'src/**/*.css',
+      '!jquery.fancybox-*.css',
+      '!jquery.dataTables.css',
+      '!jquery.fileupload-ui.css'
+    ]);
+
+  var source = gulp.src('src/main/webapp/themes/kboot/stylesheets/kboot.less')
+    .pipe(less());
+
+  return merge(vendor, source)
     .pipe(prefix())
     .pipe(concat('bundle.css'))
     .pipe(gulp.dest('./dist/assets'))
