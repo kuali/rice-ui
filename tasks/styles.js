8de61ffccbedc2b2da374d9bb4de06cbab51330f
Chris Basham <csbasham@iu.edu>
2014-09-22 11:43:45 -0400

    Cleaned styles task, and removed unused dependencies.

--- tasks/styles.js
+++ tasks/styles.js
@@ -1,12 +1,8 @@
 var gulp = require('gulp');
 var filter = require('gulp-filter');
-var order = require('gulp-order');
-var debug = require('gulp-debug');
 var less = require('gulp-less');
+var order = require('gulp-order');
 var prefix = require('gulp-autoprefixer');
-var rebase = require('gulp-css-rebase-urls');
-var replace = require('gulp-replace');
-var csso = require('gulp-csso');
 var concat = require('gulp-concat');
 var browserSync = require('browser-sync');
 
@@ -51,16 +47,8 @@ gulp.task('styles', function() {
     .pipe(order(srcOrder))
     // Post-processor transformations.
     .pipe(prefix())
-    // Cleanup some comments that will cause Rebase to error.
-    //.pipe(csso())
+    // Bundle and output styles.
     .pipe(concat('bundle.css'))
-    //.pipe(rebase())
-    //.pipe(replace(/src\/main\/webapp\//g, ''))
-    // Fix output from Rebase.
-    //.pipe(csso())
-    // Confirm the source order via terminal log.
-    .pipe(debug())
-    // Output bundled styles.
     .pipe(gulp.dest('./dist/assets'))
     // Inject CSS into browser.
     .pipe(browserSync.reload({ stream: true }));
